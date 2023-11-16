773e15411afeb18e87265698206d9d4a28aabca2
akramhany <akramhany65@gmail.com>
2023-11-16 07:56:34 -0800

    wrap return value with ()

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	newNode = malloc(sizeof(list_t));
 
 	if (!newNode)
-		return NULL;
+		return (NULL);
 
 	newNode->str = strdup(str);
 	newNode->len = len;
