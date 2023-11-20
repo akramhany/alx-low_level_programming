7f6ec2e304590ad45d89f8e715cad2546f710325
akramhany <akramhany65@gmail.com>
2023-11-20 04:08:41 -0800

    implement add_nodeint function and add a test file for it (2-main.c)

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint - add a new node at the beginning of a linked list
+ * @head: the head of the linked list
+ * @n: the value that would be stored in the new node
+ * Return: address of the new node
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNodePtr = NULL;
+
+	newNodePtr = malloc(sizeof(listint_t));
+	
+	if (newNodePtr == NULL)
+		return (NULL);
+	
+	newNodePtr->n = n;
+	newNodePtr->next = (*head);
+	(*head) = newNodePtr;
+
+	return (*head);
+}
