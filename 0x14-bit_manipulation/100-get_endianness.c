7dbb0aae76342df2965aa337950b371d79250981
akramhany <akramhany65@gmail.com>
2023-11-24 04:56:37 -0800

    task 1 advanced solution and test

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * get_endianness - gets the endianness of the machine
+ * Return: 0 if its big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	int x = 1;
+	char *y = (char*)(&x);
+
+	if (*y + 48 == '1')
+		return (1);
+	else
+		return (0);
+}
