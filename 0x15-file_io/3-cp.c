1133f52cd0cf414591b92d2a32f8c08dace9b90c
akramhany <akramhany65@gmail.com>
2023-11-28 02:16:21 -0800

    task 3 solution styling

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,8 +3,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/**
+ * CheckFileTo - takes a value and check if there are errors
+ * @num: the value to check for (-1 is an error)
+ * @op: type of operation (read, write, open, close)
+ * @fileName: the file name which had the error
+ * Return: void
+ */
+
 void CheckFileTo(int num, char op, char *fileName)
-{	
+{
 	if (num == -1 && (op == 'W' || op == 'O'))
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fileName);
@@ -16,6 +24,14 @@ void CheckFileTo(int num, char op, char *fileName)
 		exit(100);
 	}
 }
+
+/**
+ * CheckFileFrom - takes a value and check if there are errors
+ * @num: the value to check for (-1 is an error)
+ * @op: type of operation (read, write, open, close)
+ * @fileName: the file name which had the error
+ * Return: void
+ */
 
 void CheckFileFrom(int num, char op, char *fileName)
 {
@@ -31,11 +47,17 @@ void CheckFileFrom(int num, char op, char *fileName)
 	}
 }
 
+/**
+ * main - copies the content of a file to another file
+ * @argc: num of arguments
+ * @argv: given files names
+ * Return: -1 if error
+ */
+
 int main(int argc, char **argv)
 {
-	int fileTo, fileFrom, lettersRead = -1, lettersWritten = -1;
+	int fileTo, fileFrom, lettersRead = -1, lettersWritten = -1, temp1, temp2;
 	char *buffer;
-	int temp1, temp2;
 	char tempC1, tempC2;
 
 	if (argc != 3)
@@ -46,37 +68,30 @@ int main(int argc, char **argv)
 
 	buffer = malloc(1024);
 
-	fileFrom = open(argv[1], O_RDONLY);
-	
+	fileFrom = open(argv[1], O_RDONLY)
 	CheckFileFrom(fileFrom, 'O', argv[1]);
 
 	fileTo = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-
 	CheckFileTo(fileTo, 'O', argv[2]);
 
 	while (lettersRead != 0)
 	{
 		lettersRead = read(fileFrom, buffer, 1024);
-
 		CheckFileFrom(lettersRead, 'R', argv[1]);
 
 		lettersWritten = write(fileTo, buffer, lettersRead);
-
 		CheckFileTo(lettersWritten, 'W', argv[2]);
 	}
-
 
 	free(buffer);
 	temp1 = close(fileFrom);
 
 	tempC1 = fileFrom + '0';
-
 	CheckFileFrom(temp1, 'C', &tempC1);
 
 	temp2 = close(fileTo);
 
 	tempC2 = fileTo + '0';
-
 	CheckFileTo(temp2, 'C', &tempC2);
 
 	return (0);
