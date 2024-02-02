4b2231689ca2ce3939d2a4a1cbe5cba405f69383
akramhany <akramhany65@gmail.com>
2024-02-02 15:39:01 +0200

    feat_Task0 implementation

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of a doubly linked list.
+ * @h: the d linked list.
+ * Return: the number of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int numOfNodes = 0;
+
+	if (h == NULL)
+	{
+		return 0;
+	}
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		numOfNodes++;
+	}
+
+	return (numOfNodes);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LISTS.H
-#define LISTS.H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -19,5 +19,7 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
 
 #endif
