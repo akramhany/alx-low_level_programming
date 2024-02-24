6bbe1a2ae237454f67dc6bdab7981c500e33e5b5
akramhany <akramhany65@gmail.com>
2024-02-24 17:15:38 +0200

    feat: Task 3 edit

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,6 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((const unsigned char *) key, ht->size);
 	hash_node_t *new_node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	hash_node_t *ptr = NULL;
 
 	if (new_node == NULL)
 		return (0);
@@ -24,16 +25,36 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	strcpy(new_node->key, key);
 	strcpy(new_node->value, value);
 
-	if (ht->array[index] == NULL)
+	ptr = ht->array[index];
+	if (ptr == NULL)
 	{
 		ht->array[index] = new_node;
 		new_node->next = NULL;
+		return (1);
 	}
-	else
+
+	if (strcmp(ptr->key, key) == 0)
 	{
-		new_node->next = ht->array[index];
+		free(ptr);
 		ht->array[index] = new_node;
+		new_node->next = NULL;
+		return (1);
 	}
+
+	while (ptr->next)
+	{
+		if (strcmp(ptr->next->key, key) == 0)
+		{
+			new_node->next = ptr->next->next;
+			free(ptr->next);
+			ptr->next = new_node;
+			return (1);
+		}
+		ptr = ptr->next;
+	}
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
 
 	return (1);
 }
